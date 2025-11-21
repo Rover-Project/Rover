ef7addd2999a90d92f88f67c749f6199a2d9d228
Francisco Sarmento Neto <sarmento.neto.francisco@gmail.com>
2025-11-21 10:28:27 -0300

    Modify image capture script for keyboard controls
    Updated the image capture script to use keyboard inputs for capturing images and exiting the program.

--- driver_camera/scripts_teste/captura_imagem_simples.py
+++ driver_camera/scripts_teste/captura_imagem_simples.py
@@ -1,34 +1,38 @@
+
+
 from picamera2 import Picamera2, Transform
 from datetime import datetime
 import os
+import keyboard
 
-# Inicializa a câmera
 picam2 = Picamera2()
-rotacao = 90
-cfg = picam2.create_still_configuration(main={"size": (3280, 2464)}, transform=Transform(rotation=rotacao))
+
+rotacao = 90  # ALTERE PARA 0, 90, 180 OU 270
+
+cfg = picam2.create_still_configuration(
+    main={"size": (3280, 2464)},
+    transform=Transform(rotation=rotacao)
+)
+
 picam2.configure(cfg)
 picam2.start()
 
-# Pasta onde as fotos serão salvas
 pasta = "/home/rover/Imagens/TestesDoNovoSensor"
 os.makedirs(pasta, exist_ok=True)
 
-print("=== Sistema de Captura de Fotos ===")
-print("Pressione ENTER para capturar uma foto.")
-print("Digite 'q' e pressione ENTER para sair.\n")
+print("Pressione ESPAÇO para capturar, 'q' para sair.")
 
 while True:
-    comando = input("Comando: ")
+    if keyboard.is_pressed('space'):
+        nome = datetime.now().strftime("img_%Y%m%d_%H%M%S.jpg")
+        caminho = os.path.join(pasta, nome)
+        picam2.capture_file(caminho)
+        print("Foto salva:", caminho)
 
-    if comando.lower() == "q":
-        print("Encerrando o programa...")
+        while keyboard.is_pressed('space'):
+            pass  # impede múltiplos disparos
+
+    if keyboard.is_pressed('q'):
         break
-
-    # Capturar foto com data e hora
-    nome = datetime.now().strftime("img_%Y%m%d_%H%M%S.jpg")
-    caminho = os.path.join(pasta, nome)
-
-    picam2.capture_file(caminho)
-    print(f"Foto capturada e salva em: {caminho}")
 
 picam2.stop()
