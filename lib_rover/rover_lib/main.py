e7ee1c958f2b18cb7f216217b0de15b9be3a5974
AbstractGleidson <gleidsonabstract.evgm@gmail.com>
2025-12-09 09:02:39 -0300

    debug

--- lib_rover/rover_lib/main.py
+++ lib_rover/rover_lib/main.py
@@ -5,23 +5,23 @@ from circleDetect import CircleDetect
 
 if __name__ == "__main__":
     HEIGHT = 640
-    WIDTH = 480 
+    WIDTH = 480
     THRES = 10  # Delimita um espaço no eixo X para considerar o centro
     SPEED = 50  # Velocidade de rotação
-    
+
     circleDetect = CircleDetect()
     circleDetect.cameraStart(HEIGHT, WIDTH)  # Inicia a câmera
-    
+
     pins_motors = Config.get("gpio")
-    
+
     letf = (int(pins_motors["motor_esquerdo"]["in1"]), int(pins_motors["motor_esquerdo"]["in2"]))
     right = (int(pins_motors["motor_direito"]["in3"]), int(pins_motors["motor_direito"]["in4"]))
-    
+
     robot = Robot(
         left=letf,
         right=right
     )
-    
+
     x_center = {
         "low": (WIDTH // 2) - THRES,
         "high": (WIDTH // 2) + THRES
@@ -29,24 +29,26 @@ if __name__ == "__main__":
 
     while True:
         circles = circleDetect.getCircle()
-        
+
         if circles is None:
             print("Nenhum circulo foi detectado")
             robot.turn_right(SPEED)  # Rotaciona procurando um círculo
-        
+
         else:
             if len(circles) > 1:
                 print("Multiplos circulos detectados")
                 robot.stop()
             else:
-                x, y, r = circles[0]  # Agora circles[0] já contém (x, y, r)
-                
-                print(f"Circulo unico detectado: centro - ({x}, {y}); raio - {r}")
-                
-                if x > x_center["high"]:
-                    robot.turn_right(SPEED)
+                print(circles)
+                robot.stop()
+                # x, y, r = circles[0]  # Agora circles[0] já contém (x, y, r)
 
-                elif x < x_center["low"]:
-                    robot.turn_left(SPEED)
-        
+                # print(f"Circulo unico detectado: centro - ({x}, {y}); raio - {r}")
+
+                # if x > x_center["high"]:
+                #     robot.turn_right(SPEED)
+
+                # elif x < x_center["low"]:
+                #     robot.turn_left(SPEED)
+
         time.sleep(1)
