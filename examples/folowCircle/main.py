fb06d70002385ed8c8083572287e2854b85546e2
S0vina <hillan1236@gmail.com>
2025-12-15 16:46:00 -0300

    Improve Rover movement smoothing for 'follow circle' task. close #19

--- examples/folowCircle/main.py
+++ examples/folowCircle/main.py
@@ -13,7 +13,11 @@ if __name__ == "__main__":
     HEIGHT = 640 # Altura da imagem 
     WIDTH = 640 # Largura da imagem
     SPEED = 100  # Velocidade de rotação
-    CENTER_THRES = 100 # Limiar de tolerencia para o centro
+    BALANCING_ROTACION_H = 5 / 10 # Balanceamento para rotação no sentido horário
+    BALANCING_ROTACION_ANTH = 6 / 10 # Balanceamento para rotação no sentido ant-horário
+    BALANCING_MOTOR_RIGHT = 1 # Balanceamento para o motor direito 
+    BALANCING_MOTOR_LEFT = 7 / 10 # Balanceamento para o motor esquerdo
+    CENTER_THRES = 250 # Limiar de tolerencia para o centro
     RED_THRES_LOW = 200000 # Limite inferior para a detecção de vermelho
     RED_THRES_UPPER = 400000 # Limite superior para a detecção de vermelho
     CIRCLE_THRES = 40 # Limiar para considerar uma dada bola a mesma 
@@ -41,6 +45,7 @@ if __name__ == "__main__":
 
     # Centro do frame no eixo x
     x_center = WIDTH // 2
+    pause = True
 
     # Loop principal de movimento
     while True:
@@ -93,34 +98,46 @@ if __name__ == "__main__":
         openCv.imshow("Deteccao Final", frame)
         openCv.imshow("Mascara", mask)
 
-        if openCv.waitKey(1) & 0xFF == ord('q'):
+        key = openCv.waitKey(1) & 0xFF 
+
+        if key == ord('q'):
             break
+        
+        elif key == ord("p"):
+            pause = True
+        
+        elif key == ord("s"):
+            pause = False
 
         # calcula a quantidade de vermelho na cena
         red_area = openCv.countNonZero(mask)
 
-        if circleHistory is None:
-            print(f"Area vermelha: {red_area}")
-            if red_area >= RED_THRES_LOW and RED_THRES_UPPER > red_area: # Chegou perto o suficiente da bola
-                robot.stop()
-            elif last_circle:
-                last_x, last_y, last_r = last_circle
-                if last_x > WIDTH // 2:
-                    robot.move(60, -60)  # gira para direita
+        # Se não tiver em modo pausa pode acionar as operações de movimento
+        if not pause:
+            if circleHistory is None:
+                print(f"Area vermelha: {red_area}")
+                if red_area >= RED_THRES_LOW and RED_THRES_UPPER > red_area: # Chegou perto o suficiente da bola
+                    robot.stop()
+                elif last_circle:
+                    last_x, last_y, last_r = last_circle
+                    if last_x > x_center:
+                        robot.move(speed_left=SPEED * BALANCING_ROTACION_H, speed_right=-SPEED * BALANCING_ROTACION_H)  # gira para direita
+                    else:
+                        robot.move(speed_left=-SPEED * BALANCING_ROTACION_ANTH, speed_right=SPEED * BALANCING_ROTACION_ANTH)  # gira para esquerda
                 else:
-                    robot.move(-60, 60)  # gira para esquerda
-            else:
-                print("Nenhum circulo foi detectado")
-                robot.move(-60, 60)  # rotaciona procurando um círculo
+                    print("Nenhum circulo foi detectado")
+                    robot.move(speed_left=-SPEED * BALANCING_ROTACION_ANTH, speed_right=SPEED * BALANCING_ROTACION_ANTH)  # rotaciona procurando um círculo
 
-        else:
-            x, y, r = circleHistory
-            if x > x_center + CENTER_THRES:
-                robot.move(60, -60)
-            elif x < x_center - CENTER_THRES:
-                robot.move(-60, 60)
             else:
-                robot.move(70, 100)
+                x, y, r = circleHistory
+                if x > x_center + CENTER_THRES:
+                    robot.move(speed_left=SPEED * BALANCING_ROTACION_H, speed_right=-SPEED * BALANCING_ROTACION_H)
+                elif x < x_center - CENTER_THRES:
+                    robot.move(speed_left=-SPEED * BALANCING_ROTACION_ANTH, speed_right=SPEED * BALANCING_ROTACION_ANTH)
+                else:
+                    robot.move(speed_left=SPEED * BALANCING_MOTOR_LEFT,speed_right=SPEED * BALANCING_MOTOR_RIGHT)
+        else:
+            robot.stop()
 
         time.sleep(0.1)
 
