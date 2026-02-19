14e2d42305b090f29b00824023df7606260b0503
AbstractGleidson <gleidsonabstract.evgm@gmail.com>
2026-02-19 11:10:10 -0300

    teste

--- examples/circleDetect/src/circleDetect.py
+++ examples/circleDetect/src/circleDetect.py
@@ -51,7 +51,7 @@ def smoothDetect():
     )
     camera.start()
     
-    camera.set_brightness(0.05) # 0.5 de brilho fica muito bom
+    camera.set_brightness(0.25) # 0.5 de brilho fica muito bom
     camera.set_contrast(1)
     camera.set_saturation(1)
 
