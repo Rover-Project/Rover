bff6b99308a551dad5b02be1c1462c79073b232c
AbstractGleidson <gleidsonabstract.evgm@gmail.com>
2026-02-19 11:32:40 -0300

    example getVideo

---
+++ examples/getVideo/main.py
@@ -0,0 +1,29 @@
+from roverlib.plugins.camera.camera import Camera
+from pathlib import Path
+import sys
+
+try:
+    file = sys.argv[1]
+    duration = int(sys.argv[2])
+except:
+    print("Você não passou os parâmetros de linha de comando. file: caminho, duration: duração")
+
+if __name__ == "__main__":
+    
+    home = Path.home()
+    
+    path = str(home / "Vídeos" / file) 
+    
+    camera = Camera(
+        height=3840,
+        width=2160,
+        fps=30,
+    ) 
+    
+    camera.start()
+    # Configuração base de captura
+    camera.set_brightness(0) 
+    camera.set_contrast(1)
+    camera.set_saturation(5)
+    
+    camera.get_video(file=path, duration=duration)
