acaff668e7a9285d6c8159481538f06da364ed54
AbstractGleidson <gleidsonabstract.evgm@gmail.com>
2026-02-23 16:54:03 -0300

    teste

--- examples/folowCircle/folowCircle.py
+++ examples/folowCircle/folowCircle.py
@@ -56,7 +56,7 @@ def folowCircle():
     RED_THRES_UPPER = 400000 # Limite superior para a detecção de vermelho
     CIRCLE_THRES = 40  # tolerância para considerar mesma circuferencia
     NO_DET_LIMIT = 10  # número máximo de frames sem detecção
-    BASE_SPEED = 50 # Velocidade base para controle
+    BASE_SPEED = 60 # Velocidade base para controle
     SEARCH_SPEED = 70
     last_circle = None  # guarda o ulthimo circulo detectado
     circleHistory = None  # média acumulada, para suavizar as mudanças de posição do circulo
