89023913f2a7f377cd164e969364ad879cc9b010
S0vina <hillan1236@gmail.com>
2026-02-25 16:19:36 -0300

    Corrections

--- examples/lineDetect/main.py
+++ examples/lineDetect/main.py
@@ -324,7 +324,7 @@ if __name__ == "__main__":
                 openCV.circle(result, (centro_poligono, y_max - 20), 10, (255, 0, 0), -1) # desenha uma bola no centro da estrada
 
                 # Exibicao das telas
-            openCV.imshow("Navegacao Rover", frame_linhas)
+            openCV.imshow("Navegacao Rover", result)
             openCV.imshow("ROI", roi)
             
             # --- ADICIONE ESTA PARTE AQUI ---
