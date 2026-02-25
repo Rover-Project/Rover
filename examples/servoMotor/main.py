7d6e2067aa3a350e043834d16fb153b00c4425e3
AbstractGleidson <gleidsonabstract.evgm@gmail.com>
2026-02-25 16:36:37 -0300

    servo motor teste

---
+++ examples/servoMotor/main.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+
+# Cria PWM em 50Hz (servo padrão)
+pwm = GPIO.PWM(18, 50)
+pwm.start(0)
+
+def set_angle(angle):
+    # Converte ângulo (0–180) em duty cycle
+    duty = 2 + (angle / 18)
+    GPIO.output(18, True)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.5)
+    GPIO.output(18, False)
+    pwm.ChangeDutyCycle(0)
+
+try:
+    while True:
+        for angle in [0, 45, 90, 100, 120]:
+            print(f"Movendo para {angle} graus")
+            set_angle(angle)
+            time.sleep(1)
+
+except KeyboardInterrupt:
+    pwm.stop()
+    GPIO.cleanup()
