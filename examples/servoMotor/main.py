6440873eb48756b23957c895bf7979ab4e533ec0
AbstractGleidson <gleidsonabstract.evgm@gmail.com>
2026-03-09 15:53:03 -0300

    Servo plugin implemented

--- examples/servoMotor/main.py
+++ examples/servoMotor/main.py
@@ -1,52 +1,28 @@
-import RPi.GPIO as GPIO
+from roverlib.plugins.servoMotor.servo import Servo
 import time
 
-servo180 = 23
-servo360 = 24
+if __name__ == "__main__":
+    pin180 = 23
+    pin360 = 24
 
-# Configura o servo 180
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo180, GPIO.OUT)
+    servo180 = Servo(pin180)
+    servo180.start()
+    
+    servo360 = Servo(pin360)
+    servo360.start()
 
-# Configura o servo 360
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo360, GPIO.OUT)
+    servo180.set_angle(0)
+    servo180.set_angle(0)
+    last_angle = 0
+    
+    try:
+        while True:
+            angle = float(input("Qual angulo deseja?: "))
+            servo180.set_smooth_angle(last_angle, angle)
+            time.sleep(1)
+            servo360.set_smooth_angle(last_angle, angle)
+            time.sleep(1)
 
-# Cria PWM em 50Hz para o servo 180
-pwm180 = GPIO.PWM(servo180, 50)
-pwm180.start(0)
-
-# Cria PWM em 50Hz para o servo 360
-pwm360 = GPIO.PWM(servo360, 50)
-pwm360.start(0)
-
-def set_angle180(angle):
-    # Converte ângulo (0–180) em duty cycle
-    duty = 2 + (angle / 18)
-    GPIO.output(servo180, True)
-    pwm180.ChangeDutyCycle(duty)
-    time.sleep(0.5)
-    GPIO.output(servo180, False)
-    pwm180.ChangeDutyCycle(0)
-
-def set_angle360(angle):
-    # Converte ângulo (0–180) em duty cycle
-    duty = 2 + (angle / 18)
-    GPIO.output(servo360, True)
-    pwm360.ChangeDutyCycle(duty * 2)
-    time.sleep(0.5)
-    GPIO.output(servo360, False)
-    pwm360.ChangeDutyCycle(0)
-
-try:
-    while True:
-        angle = float(input("Qual angulo deseja?: "))
-        set_angle180(angle)
-        time.sleep(1)
-        set_angle360(angle)
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    pwm180.stop()
-    pwm360.stop()
-    GPIO.cleanup()
+    except KeyboardInterrupt:
+        servo180.stop()
+        servo360.stop()
