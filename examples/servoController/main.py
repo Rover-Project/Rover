145e73d0d87119f6119630080fba48b246e0975b
AbstractGleidson <gleidsonabstract.evgm@gmail.com>
2026-03-09 14:28:15 -0300

    teste

--- examples/servoController/main.py
+++ examples/servoController/main.py
@@ -8,8 +8,8 @@ pin180 = 23
 pin360 = 24
 
 # Configuração dos Servos
-servo_a = Servo(pin180) #n360
-servo_b = Servo(pin360) #n90
+servo_a = Servo(pin180) 
+servo_b = Servo(pin360) 
 
 pos_a = 0.0
 pos_b = 0.0
@@ -34,14 +34,21 @@ try:
         if tecla == 'q': break
 
         # Movimentação
-        if tecla == 'w': pos_a = min(1.0, pos_a + passo)
-        elif tecla == 's': pos_a = max(-1.0, pos_a - passo)
-        if tecla == 'i': pos_b = min(1.0, pos_b + passo)
-        elif tecla == 'k': pos_b = max(-1.0, pos_b - passo)
+        if tecla == 'w': 
+            pos_a = min(1.0, pos_a + passo)
+        elif tecla == 's': 
+            pos_a = max(-1.0, pos_a - passo)
+        if tecla == 'i': 
+            pos_b = min(1.0, pos_b + passo)
+        elif tecla == 'k': 
+            pos_b = max(-1.0, pos_b - passo)
 
         # 1. Envia o sinal para mover
         servo_a.value = pos_a
         servo_b.value = pos_b
+        
+        print(f"Posição A: {pos_a}")
+        print(f"Posição B: {pos_b}")
         
         # 2. Espera o motor chegar na posição (ajuste se o movimento for longo)
         sleep(0.3) 
