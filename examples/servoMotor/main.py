4f9e8772e5de488ac2ffcf03968b03f82a7dc119
AbstractGleidson <gleidsonabstract.evgm@gmail.com>
2026-03-09 16:12:15 -0300

    teste

--- examples/servoMotor/main.py
+++ examples/servoMotor/main.py
@@ -5,26 +5,18 @@ import time
 
 if __name__ == "__main__":
     pin180 = 23
-    pin360 = 24
 
     servo180 = Servo180(pin180)
     servo180.start()
-    
-    servo360 = Servo360(pin360)
-    servo360.start()
 
-    servo180.set_angle(0)
     servo180.set_angle(0)
     last_angle = 0
     
     try:
         while True:
             angle = int(input("Qual angulo deseja?: "))
-            servo180.set_smooth_angle(last_angle, angle)
-            time.sleep(1)
-            servo360.set_smooth_speed(last_angle, angle)
+            servo180.set_angle(angle)
             time.sleep(1)
 
     except KeyboardInterrupt:
         servo180.stop()
-        servo360.stop()
