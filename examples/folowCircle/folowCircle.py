6dc02479700cae423fff954377b04378e0fdf192
AbstractGleidson <gleidsonabstract.evgm@gmail.com>
2026-03-12 17:00:51 -0300

    teste

--- examples/folowCircle/folowCircle.py
+++ examples/folowCircle/folowCircle.py
@@ -157,7 +157,7 @@ def folowCircle():
             error_x = x - x_center # Velocidade para centralizar a bola
             error_r = r - max_r # Velocidade para correr atrás da bola
             
-            if r >= 150:
+            if r >= 100:
                 right = 0
                 left = 0
             else:
