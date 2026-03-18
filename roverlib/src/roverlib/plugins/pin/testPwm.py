2b1e163d5d5bb54c7b82293503617423d4426141
Sarmento <sarmentoneto@acad.ifma.edu.br>
2026-03-18 16:22:28 -0300

    teste tirando sleep

--- roverlib/src/roverlib/plugins/pin/testPwm.py
+++ roverlib/src/roverlib/plugins/pin/testPwm.py
@@ -9,15 +9,15 @@ try:
 
         print("PWM 10%")
         led.pwm(0.1)
-        
+        time.sleep(2)
 
         print("PWM 50%")
         led.pwm(0.5)
-        
+        time.sleep(2)
 
         print("PWM 90%")
         led.pwm(0.9)
-        
+        time.sleep(2)
 
 except KeyboardInterrupt:
     print('Encerrando...')
