25cab3d5d41912667f6c5f4841bc6256842c3bef
Gleidson <gleidsonabstract.evgm@gmail.com>
2026-04-25 14:15:33 -0300

    PCA teste

--- examples/PCAServos/main.py
+++ examples/PCAServos/main.py
@@ -3,42 +3,51 @@ import board
 import busio
 from adafruit_pca9685 import PCA9685
 
-# Inicializa I2C
 i2c = busio.I2C(board.SCL, board.SDA)
-
-# Inicializa PCA9685
 pca = PCA9685(i2c)
-pca.frequency = 50  # 50Hz padrão para servo
+pca.frequency = 50
 
-# Função para controlar servo contínuo
 def set_servo_speed(channel, speed):
-    """
-    speed: -1.0 (máx reverso) até 1.0 (máx frente)
-    0 = parado
-    """
-    neutral = 375  
-    range_val = 100  
-
+    neutral = 375
+    range_val = 100
     pulse = int(neutral + speed * range_val)
     pca.channels[channel].duty_cycle = pulse
 
+def parar(ch):
+    set_servo_speed(ch, 0)
+
+def frente(ch):
+    set_servo_speed(ch, 0.5)
+
+def tras(ch):
+    set_servo_speed(0, -0.5)
+
 try:
     while True:
-        print("Parado")
-        set_servo_speed(0, 0)
-        set_servo_speed(1, 0)
-        time.sleep(2)
+        ch = int(input("Canal do servo: "))
+        cmd = input("Digite comando (f/t/p/q): ").lower()
 
-        print("Girando pra frente")
-        set_servo_speed(0, 0.5)
-        set_servo_speed(1, 0.5)
-        time.sleep(3)
-
-        print("Girando pra trás")
-        set_servo_speed(0, -0.5)
-        set_servo_speed(1, -0.5)
-        time.sleep(3)
+        if cmd == "f":
+            frente(ch)
+            time.sleep(0.5)
+            parar(ch)
+            
+        elif cmd == "t":
+            tras(ch)
+            time.sleep(0.5)
+            parar(ch)
+            
+        elif cmd == "p":
+            parar(ch)
+            
+        elif cmd == "s":
+            break
+        else:
+            print("Comando inválido")
 
 except KeyboardInterrupt:
     print("Encerrando")
+
+finally:
+    parar()
     pca.deinit()
