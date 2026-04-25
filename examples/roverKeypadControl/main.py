fcb3f3b9aaf23294c8c2d03cf882c35297b4a6f3
Gleidson <gleidsonabstract.evgm@gmail.com>
2026-04-25 12:16:27 -0300

    Teste

--- examples/roverKeypadControl/main.py
+++ examples/roverKeypadControl/main.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     speed = 50 # Velocidade inicial
     
     try:
-        camera = AfCameragt(HEIGHT, WIDTH)
+        camera = AfCamera(HEIGHT, WIDTH)
         camera.start()
 
     except:
