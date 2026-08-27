fe9f6e268cefd9e4530036fc52c0f5cdf47e2152
Gleidson <gleidsonabstract.evgm@gmail.com>
2026-08-27 16:25:26 -0300

    correction parameters

--- examples/folowCircle/src/error_signal.py
+++ examples/folowCircle/src/error_signal.py
@@ -9,11 +9,7 @@ def smooth_signal(current_error: float, last_error: float, alph: float) -> float
 def activation_function(raw_speed: float, min_deadzone: float) -> float:
   """Aplica o limiar mínimo de acionamento (40 e 30) remapeando a escala 0-100."""
  
-  sign = 1.0 if raw_speed > 0 else -1.0 # verifica o sinal do pulso
-
-  # Mapeia linearmente a saída do PID para iniciar acima do valor de atrito mínimo
-  scaled = min_deadzone + (abs_speed / 100.0) * (100.0 - min_deadzone)
-  return sign * scaled
+  return raw_speed if abs(raw_speed) > min_deadzone else 0
 
 def activation_deadzone(error: float, deadzone: float):
   return error if abs(error) > deadzone else 0
